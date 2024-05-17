475d2a46f1744277876e21cd761f9e2e44b54737
Jae-Hong-Kim <newfly101@naver.com>
2024-05-17 15:44:09 +0900

    Section 추가. return은 한개의 부모 element만 반환해야 한다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import TextComponents from "./components/TextComponents";
+import Section from "./components/Section";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
         <TextComponents />
+        <Section />
       </header>
     </div>
   );
---
+++ src/components/Section.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Section = () => {
+    return (
+        <div>
+            <div> 이건 div</div>
+            <p> 이건 p</p>
+        </div>
+    );
+};
+
+export default Section;
