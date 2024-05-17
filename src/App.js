2a2329d594ca8ffdaedba2e31f76b679a91e7357
Jae-Hong-Kim <newfly101@naver.com>
2024-05-17 16:00:00 +0900

    CSS 추가 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <div className="toast">
+                <main className="toast__message">
+                    <header className="toast__message-title"> success </header>
+                    <p className="toast__message-text">Right! this!! on!!</p>
+                </main>
+                <button className="toast__button">Dismiss</button>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    );
 }
 
 export default App;
