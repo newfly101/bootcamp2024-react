eef21c1b7e46e7c1e495c41738ac563de1a15cf5
Jae-Hong-Kim <newfly101@naver.com>
2024-05-17 16:03:49 +0900

    CSS 상황별 조정 완료

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import './App.css';
 function App() {
     return (
         <div className="wrapper">
-            <div className="toast">
+            <div className="toast toast-success">
                 <main className="toast__message">
-                    <header className="toast__message-title"> success </header>
-                    <p className="toast__message-text">Right! this!! on!!</p>
+                    <header className="toast__message-title"> success</header>
+                    <p className="toast__message-text">Right on! Your account has been updated.</p>
                 </main>
                 <button className="toast__button">Dismiss</button>
             </div>
-            <div></div>
-            <div></div>
+
+            <div className="toast toast-warning">
+                <main className="toast__message">
+                    <header className="toast__message-title">Warning</header>
+                    <p className="toast__message-text">Hmmm. something doesn't look right.</p>
+                </main>
+                <button className="toast__button">Dismiss</button>
+            </div>
+
+            <div className="toast toast-error">
+                <main className="toast__message">
+                    <header className="toast__message-title">Error</header>
+                    <p className="toast__message-text">Uh oh! Something went terribly wrong!</p>
+                </main>
+                <button className="toast__button">Dismiss</button>
+            </div>
         </div>
     );
 }
