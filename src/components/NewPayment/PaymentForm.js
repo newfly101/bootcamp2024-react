56e116fb8a3f60daba8380e252b21ad6b08e9861
Jae-Hong-Kim <newfly101@naver.com>
2024-05-23 20:46:19 +0900

    [28강] state를 각각 추가하는 방법

--- src/components/NewPayment/PaymentForm.js
+++ src/components/NewPayment/PaymentForm.js
@@ -3,24 +3,48 @@ import React from 'react';
 import './PaymentForm.css';
 
 const PaymentForm = () => {
+    const [name, setName] = React.useState('');
+    const [price, setPrice] = React.useState('');
+    const [todate, setTodate] = React.useState('');
+
+    const changeNameHandle = (event) => {
+        console.log(event.target.value);
+        setName(event.target.value);
+    }
+    const changePriceHandle = (event) => {
+        setPrice(event.target.value);
+    }
+
+    const changeToDateHandle = (event) => {
+        setTodate(event.target.value);
+    }
+
+    const buttonSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log(name);
+        console.log(price);
+        console.log(todate);
+    }
+
     return (
         <form>
             <div className='new-payment__controls'>
                 <div className='new-payment__control'>
                     <label>이름</label>
-                    <input type='text' />
+                    <input type='text'  onChange={changeNameHandle} />
                 </div>
                 <div className='new-payment__control'>
                     <label>금액</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' min='0.01' step='0.01' onChange={changePriceHandle} />
                 </div>
                 <div className='new-payment__control'>
                     <label>날짜</label>
-                    <input type='date' min='2019-01-01' max='2024-12-31' />
+                    <input type='date' min='2019-01-01' max='2024-12-31'
+                           onChange={changeToDateHandle}/>
                 </div>
             </div>
             <div className='new-payment__actions'>
-                <button type='submit'>결제 추가</button>
+                <button type='submit' onClick={buttonSubmitHandler}>결제 추가</button>
             </div>
         </form>
     );
