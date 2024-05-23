525c69695de5639e0106f5f71bfd6545a29644f1
Jae-Hong-Kim <newfly101@naver.com>
2024-05-23 13:43:44 +0900

    [25강] event처리

--- src/components/ToastButton.js
+++ src/components/ToastButton.js
@@ -4,7 +4,7 @@ import "./Toast.css";
 const ToastButton = () => {
     return (
         <div>
-            <button className="toast__button">Dismiss</button>
+            <button className="toast__button" onClick={() => console.log('event console!')}>Dismiss</button>
         </div>
     );
 };
