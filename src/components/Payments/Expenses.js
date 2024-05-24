d2bd1c61f56fb1cee871e8c50a25806e0d4103e1
Jae-Hong-Kim <newfly101@naver.com>
2024-05-25 01:19:22 +0900

    [34강] 리스트 필터링

--- src/components/Payments/Expenses.js
+++ src/components/Payments/Expenses.js
@@ -3,11 +3,16 @@ import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from './Card';
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
     console.log("Expenses",props);
     return (
         <Card className="expenses">
+            <ExpensesFilter
+                selected={props.selected}
+                onChange={props.onChange}
+            />
             {props.items.map((item, index) => (
             <ExpenseItem
                 key={`${item.id}-${index}`}
