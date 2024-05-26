0962cbd652e7a7ca68dba7e53e61e8ca590d6fda
Jae-Hong-Kim <newfly101@naver.com>
2024-05-27 08:32:35 +0900

    [35강] 동적인 스타일링 - 결과 값에 따라서 스타일링을 변경해주는 경우 (삼항 연산자로 처리)

--- src/components/CourseGoals/CourseGoalInput/CourseGaolInput.js
+++ src/components/CourseGoals/CourseGoalInput/CourseGaolInput.js
@@ -3,21 +3,32 @@ import Button from "../UI/Button/Button";
 
 const CourseGaolInput = (props) => {
     const [enteredValue, setEnteredValue] = useState('');
-
+    const [isVaild, setIsVaild] = useState(true);
     const goalInputChangeHandler = event => {
+        if (event.target.value.trim().length === 0) {
+            setIsVaild(true);
+        }
         setEnteredValue(event.target.value);
     };
 
     const formSubmitHandler = event => {
         event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsVaild(false);
+            return;
+        }
         props.onAddGoal(enteredValue);
+
     };
 
     return (
         <form onSubmit={formSubmitHandler}>
             <div className="form-control">
                 <label>목표</label>
-                <input type="text" onChange={goalInputChangeHandler} />
+                <input type="text" onChange={goalInputChangeHandler}
+                       style={{backgroundColor: isVaild ? "transparent" : 'red',
+                               borderColor: isVaild ? '#ccc' : 'red'}}
+                />
             </div>
             <Button type="submit">목표 추가하기</Button>
         </form>
