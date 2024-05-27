b8841dab04b8af9a64da69a99a86dba95bcae10e
Jae-Hong-Kim <newfly101@naver.com>
2024-05-27 09:47:51 +0900

    [36강] styled-components 로 JS in CSS 로 변경 작업 진행(예시코드)

--- src/components/CourseGoals/CourseGoalInput/CourseGaolInput.js
+++ src/components/CourseGoals/CourseGoalInput/CourseGaolInput.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Button from "../UI/Button/Button";
+import {styled} from "styled-components";
 import "./CourseGaolInput.css";
 
 const CourseGaolInput = (props) => {
@@ -29,14 +30,49 @@ const CourseGaolInput = (props) => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className={`form-control ${!isValid && enteredValue.length !== 0 && 'invalid'}`}>
-                <label>목표</label>
-                <input type="text" onChange={goalInputChangeHandler} value={enteredValue}
-                />
-            </div>
+            {/*<div className={`form-control ${!isValid && enteredValue.length !== 0 && 'invalid'}`}>*/}
+            {/*    <label>목표</label>*/}
+            {/*    <input type="text" onChange={goalInputChangeHandler} value={enteredValue}*/}
+            {/*    />*/}
+            {/*</div>*/}
+            <FormControl>
+                <FormControlLabel />
+                <FormControlInput type="text" onChange={goalInputChangeHandler} value={enteredValue}
+                inValid={isValid}/>
+            </FormControl>
             <Button type="submit">목표 추가하기</Button>
         </form>
     );
 };
 
 export default CourseGaolInput;
+
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+    padding-left: 10px;
+`;
+
+const FormControlLabel = styled.label`
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+`;
+
+const FormControlInput = styled.input`
+    display: block;
+    width: 50%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    
+    &:focus {
+        outline: none;
+        border-color: blue;
+    }
+    
+    ${props => !props.inValid && props.value.length !== 0 && `
+        background: salmon;
+        border: 1px solid red;
+    `}
+`;
