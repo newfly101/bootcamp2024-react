f2a0e6fc6da9ad6933698b0d4c8fc63a6afabfff
Jae-Hong-Kim <newfly101@naver.com>
2024-05-30 07:32:57 +0900

    [43강] 복잡한 이벤트와 상태 디자인하기 초기 상태 - 1 초기option설정

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 
 const DropDown = ({options}) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const [selectedOption, setSelectedOption] = React.useState(null);
+
+    const renderOptions = options.map((option) => {
+        return <div key={option.value}>{option.value}</div>
+    })
     return (
+        <>
         <div> 선택하기 </div>
+            {renderOptions}
+        </>
     );
 };
 
