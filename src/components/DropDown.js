04b96f2bca6a6f934c8a1f907b6ef1d66d2eddfb
Jae-Hong-Kim <newfly101@naver.com>
2024-05-30 07:34:57 +0900

    [43강] 복잡한 이벤트와 상태 디자인하기 초기 상태 - 2 isOpen 옵션 설정하기

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -5,13 +5,20 @@ const DropDown = ({options}) => {
     const [isOpen, setIsOpen] = React.useState(false);
     const [selectedOption, setSelectedOption] = React.useState(null);
 
+    const handleSelect = (e) => {
+        console.log(e);
+    }
+    const handleClick = (e) => {
+        console.log(e);
+    }
+
     const renderOptions = options.map((option) => {
         return <div key={option.value}>{option.value}</div>
     })
     return (
         <>
-        <div> 선택하기 </div>
-            {renderOptions}
+        <div> {!selectedOption ? '선택 하기' : selectedOption}</div>
+            {isOpen && <>renderOptions</>}
         </>
     );
 };
