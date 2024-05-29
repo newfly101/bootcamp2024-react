21da261b64d59112131a099428d6eef1e4d0079e
Jae-Hong-Kim <newfly101@naver.com>
2024-05-30 08:00:27 +0900

    [44강] 데이터 정렬 - Object정렬

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Expenses from "./components/Payments/Expenses";
 import {useState} from "react";
 import StyleView from "./components/CourseGoals/StyleView";
 import DropDown from "./components/DropDown";
+import SortableTable from "./components/SortableTable";
 // import AlexaImage from "./images/alexa.png";
 // import CortanaImage from "./images/cortana.png";
 // import SiriImage from "./images/siri.png";
@@ -73,6 +74,40 @@ function App() {
         {label:"파랑", value:"blue" },
     ]
 
+    const data = [
+        { name: '오렌지', color: 'bg-orange-500', score: 5 },
+        { name: '사과', color: 'bg-red-500', score: 3 },
+        { name: '바나나', color: 'bg-yellow-500', score: 1 },
+        { name: '라임', color: 'bg-green-500', score: 4 },
+        { name: '체리', color: 'bg-red-700', score: 2.5 },
+    ];
+
+    const config = [
+        {
+            label: '이름',
+            render: (fruit) => fruit.name,
+            sortValue: (fruit) => fruit.name,
+        },
+        {
+            label: '색상',
+            render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+        },
+        {
+            label: '점수',
+            render: (fruit) => fruit.score,
+            sortValue: (fruit) => fruit.score,
+        },
+        {
+            label: '점수의 제곱',
+            render: (fruit) => fruit.score ** 2,
+            sortValue: (fruit) => fruit.score ** 2,
+        },
+    ];
+
+    const keyFn = (fruit) => {
+        return fruit.name;
+    };
+
 
     return (
         <div className="wrapper">
@@ -88,6 +123,7 @@ function App() {
             {/*<img src={SiriImage} alt="Siri" width={400} height={400}/>*/}
             <StyleView />
             <DropDown options={options}/>
+            <SortableTable data={data} config={config} keyFn={keyFn} />
         </div>
     );
 }
