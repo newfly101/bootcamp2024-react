76da0d26b9cf2c924c9056c8ec7a14e9216bdb0b
Jae-Hong-Kim <newfly101@naver.com>
2024-05-30 10:38:46 +0900

    [49강] 사이드 이펙트 처리 - useEffect 실습 전 코드 추가 + App.js에서 조건 처리하기(view에서 안보이게)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
+import MainHeader from "./components/MainHeader/MainHeader";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
 
 function App() {
     const [usersList, setUsersList] = useState([]);
@@ -14,12 +17,33 @@ function App() {
             ];
         });
     };
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };
 
     return (
-        <div>
-            <AddUser onAddUser={addUserHandler} />
-            <UsersList users={usersList} />
-        </div>
+        <>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler} />}
+                {isLoggedIn && <Home onLogout={logoutHandler} />}
+            </main>
+            {isLoggedIn && <>
+                <AddUser onAddUser={addUserHandler} />
+                {usersList.length > 0 && (
+                    <UsersList users={usersList} />
+                )}
+            </>}
+
+        </>
     );
 }
 
