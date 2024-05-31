d74b444da3b446ab9088ad8a8a657be00be6743a
Jae-Hong-Kim <newfly101@naver.com>
2024-05-31 12:37:29 +0900

    [51강] 사이드 이펙트 처리 - useReducer을 이용 : button disabled안되는 문제가 있었는데 component로 만든 Button에서 disabled옵션에 주는 값이 없어서 추가해줌

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -8,6 +8,7 @@ const Button = (props) => {
             className={classes.button}
             type={props.type || 'button'}
             onClick={props.onClick}
+            disabled={props.disabled}
         >
             {props.children}
         </button>
