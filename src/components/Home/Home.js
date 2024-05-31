2145281ceab889e7c368cc3a92b628a01cdee906
Jae-Hong-Kim <newfly101@naver.com>
2024-06-01 07:31:41 +0900

    [06강] HTTPS : GET요청 : newsList라는 무료 api를 이용해서 통신하는 법 추가

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Card from '../UI/Card';
 import classes from './Home.module.css';
+import NewsList from "../NewsList";
 
 const Home = () => {
     return (
         <Card className={classes.home}>
-            <h1>환영합니다.</h1>
+            <NewsList />
         </Card>
     );
 };
