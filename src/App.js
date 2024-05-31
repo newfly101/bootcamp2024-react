3cd69447e264199c811f5686477bb93ae0b97655
Jae-Hong-Kim <newfly101@naver.com>
2024-05-31 14:23:51 +0900

    [52강] 강의에서 사용하지 않는 부분 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,60 +1,23 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
 import MainHeader from "./components/MainHeader/MainHeader";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import AuthContext from "./context/AuthContext";
 
 function App() {
-    const [usersList, setUsersList] = useState([]);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+        const context = useContext(AuthContext);
 
-    const addUserHandler = (uName, uAge) => {
-        setUsersList((prevUsersList) => {
-            return [
-                ...prevUsersList,
-                {name: uName, age: uAge, id: Math.random().toString()},
-            ];
-        });
-    };
-    // useEffect(() => {
-    //     // 로그인 되면 isLoggedIn = true [or] isLoggedIn = false
-    //     const getLogin = localStorage.getItem('isLoggedIn');
-    //     getLogin ? setIsLoggedIn(true) : setIsLoggedIn(false);
-    //     console.log("hello")
-    // }, []);
-    //
-    // const loginHandler = (email, password) => {
-    //     localStorage.setItem("isLoggedIn", email);
-    //     setIsLoggedIn(true);
-    // };
-    //
-    // const logoutHandler = () => {
-    //     localStorage.removeItem("isLoggedIn");
-    //     setIsLoggedIn(false);
-    // };
-
-    const context = useContext(AuthContext);
-
-    return (
-        <AuthContext>
-            <MainHeader />
-            {/*<MainHeader isAuthenticated={context.isLoggedIn} onLogout={context.logoutHandler}/>*/}
-            <main>
-                {!isLoggedIn && <Login onLogin={context.loginHandler}/>}
-                {isLoggedIn && <Home onLogout={context.logoutHandler}/>}
-            </main>
-            {isLoggedIn && <>
-                <AddUser onAddUser={addUserHandler}/>
-                {usersList.length > 0 && (
-                    <UsersList users={usersList}/>
-                )}
-            </>}
-
-        </AuthContext>
-    );
+        console.log("context",context); // context값 확인용
+        return (
+            <>
+                <MainHeader/>
+                <main>
+                    {!context.isLoggedIn && <Login/>}
+                    {context.isLoggedIn && <Home/>}
+                </main>
+            </>
+        );
 }
 
 export default App;
