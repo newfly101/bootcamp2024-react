caa2a89700864bfba858afe9628740a4d1a2acba
Jae-Hong-Kim <newfly101@naver.com>
2024-05-31 09:03:08 +0900

    [51강] 사이드 이펙트 처리 - useReducer을 이용 : 기존 state값을 reducer의 value와 isValid 옵션으로 전부 변경

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
 
 import Card from '../UI/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button';
 
+const emailReducer = (state, action) => {
+
+}
+
+const passwordReducer = (state, action) => {
+
+}
+
 const Login = (props) => {
-    const [enteredEmail, setEnteredEmail] = useState('');
-    const [emailIsValid, setEmailIsValid] = useState();
-    const [enteredPassword, setEnteredPassword] = useState('');
-    const [passwordIsValid, setPasswordIsValid] = useState(false);
+    const [emailState, dispatchEmail] = useReducer(emailReducer, {
+        // email의 초기값 세팅
+        value : '',
+        isValid: null,
+    });
+    const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+        value : '',
+        isValid: null,
+    });
     const [formIsValid, setFormIsValid] = useState(false);
 
     // 중복된 setFormIsValid를 useEffect를 이용해서 해당 값이 변경될 때 check하도록 함
@@ -16,7 +29,7 @@ const Login = (props) => {
         const identifier = setTimeout(() => {
             console.log("check validity");
             setFormIsValid(
-                enteredEmail.includes('@') && enteredPassword.trim().length > 6
+                emailState.value.includes('@') && passwordState.value.trim().length > 6
             );
         }, 500);
         // cleanUp function
@@ -24,28 +37,28 @@ const Login = (props) => {
             console.log("clean up");
             clearTimeout(identifier);
         }
-        console.log("length enteredPassword : ", enteredPassword.trim().length);
-    }, [enteredEmail, enteredPassword]);
+        console.log("length passwordState.value : ", passwordState.value.trim().length);
+    }, [emailState.value, passwordState.value]);
 
     const emailChangeHandler = (event) => {
-        setEnteredEmail(event.target.value);
+        // setemailState.value(event.target.value);
     };
 
     const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
+        // setpasswordState.value(event.target.value);
     };
 
     const validateEmailHandler = () => {
-        setEmailIsValid(enteredEmail.includes('@'));
+        // setemailState.isValid(emailState.value.includes('@'));
     };
 
     const validatePasswordHandler = () => {
-        setPasswordIsValid(enteredPassword.trim().length > 6);
+        // setpasswordState.isValid(passwordState.value.trim().length > 6);
     };
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(enteredEmail, enteredPassword);
+        props.onLogin(emailState.value, passwordState.value);
     };
 
     return (
@@ -53,28 +66,28 @@ const Login = (props) => {
             <form onSubmit={submitHandler}>
                 <div
                     className={`${classes.control} ${
-                        emailIsValid === false ? classes.invalid : ''
+                        emailState.isValid === false ? classes.invalid : ''
                     }`}
                 >
                     <label htmlFor="email">이메일</label>
                     <input
                         type="email"
                         id="email"
-                        value={enteredEmail}
+                        value={emailState.value}
                         onChange={emailChangeHandler}
                         onBlur={validateEmailHandler}
                     />
                 </div>
                 <div
                     className={`${classes.control} ${
-                        passwordIsValid === false ? classes.invalid : ''
+                        passwordState.isValid === false ? classes.invalid : ''
                     }`}
                 >
                     <label htmlFor="password">비밀번호</label>
                     <input
                         type="password"
                         id="password"
-                        value={enteredPassword}
+                        value={passwordState.value}
                         onChange={passwordChangeHandler}
                         onBlur={validatePasswordHandler}
                     />
