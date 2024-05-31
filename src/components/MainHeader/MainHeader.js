03a5e3dae0ce455da7938cea968225f841c8f060
Jae-Hong-Kim <newfly101@naver.com>
2024-05-31 14:12:24 +0900

    [52강] context API추가 : context를 Navigation에 추가하고 각 각 제공하는걸 내려줌2

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
+// import AuthContext from "../../context/AuthContext";
 
-const MainHeader = (props) => {
+const MainHeader = () => {
+    // const context = useContext(AuthContext);
     return (
         <header className={classes['main-header']}>
             <h1>리액트 페이지</h1>
-            <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+            <Navigation />
+            {/*<Navigation isLoggedIn={context.isLoggedIn} onLogout={context.onLogout} />*/}
         </header>
     );
 };
