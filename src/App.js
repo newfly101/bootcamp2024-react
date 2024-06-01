847f0bb8cf95425930eedc528d79c4c6eec30ebf
Jae-Hong-Kim <newfly101@naver.com>
2024-06-01 13:15:51 +0900

    [63강] 커스텀 훅 만들기 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import AuthContext from "./context/AuthContext";
+import ForwardCounter from "./components/ForwardCounter";
+import BackwardCounter from "./components/BackwardCounter";
 
 function App() {
         const context = useContext(AuthContext);
@@ -16,6 +18,8 @@ function App() {
                     {!context.isLoggedIn && <Login/>}
                     {context.isLoggedIn && <Home/>}
                 </main>
+                    <ForwardCounter />
+                    <BackwardCounter />
             </>
         );
 }
