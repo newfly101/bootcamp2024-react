3dafad8808465f65e26193bfe2b48d0a7d9eeb30
Jae-Hong-Kim <newfly101@naver.com>
2024-06-04 10:11:32 +0900

    [67강] form input - submit 눌렀을 때 에러 메시지 노출

--- src/components/Login/JoinInput.js
+++ src/components/Login/JoinInput.js
@@ -4,13 +4,16 @@ import classes from './JoinInput.module.css';
 const JoinInput = (props) => {
     const nameInputRef = useRef();
     const [enteredName, setEnteredName] = React.useState('');
+    const [isValid, setIsValid] = React.useState(true);
 
     const formSubmitHandler = (e) => {
         e.preventDefault();
         if (nameInputRef.current.value === '' || enteredName.trim().length === 0) {
-            console.error("input값 : ",enteredName);
+            setIsValid(false);
+            console.error("이름에 빈 값이 들어갔습니다");
         } else {
             // submit 한 결과 값 전달
+            setIsValid(true);
             console.log("enteredName", enteredName);
         }
     }
@@ -23,6 +26,7 @@ const JoinInput = (props) => {
                 <label htmlFor='userName'>사용자 이름</label>
                 <input type='text' id='username' ref={nameInputRef} value={enteredName}
                         onChange={(event) => setEnteredName(event.target.value)}/>
+                {!isValid && <p className={classes.errorText}>이름에 빈 값이 들어갔습니다</p>}
             </div>
             <div className={classes.formActions}>
                 <button className={classes.btn}>제출</button>
